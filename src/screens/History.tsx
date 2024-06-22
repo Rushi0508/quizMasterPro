26e0317a7ec4381f03b65faacf8f7def95795d7d
Rushi_0508 <rushigandhi14@gmail.com>
2024-06-22 15:52:22 +0530

    Added Loaders

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,7 +1,7 @@
 import { View, Text, Touchable, TouchableOpacity, ToastAndroid } from "react-native";
 import React, { useCallback, useEffect } from "react";
 import BGView from "../components/BGView";
-import { Appbar, Icon, MD3Colors } from "react-native-paper";
+import { ActivityIndicator, Appbar, Icon, MD3Colors } from "react-native-paper";
 import App, { navigationRef } from "../App";
 import { QuizAPI } from "../helpers/quiz";
 import useAuth from "../hooks/useAuth";
@@ -15,6 +15,7 @@ const History = ({ route }: any) => {
     const [quizzes, setQuizzes] = React.useState([] as any)
     const { user } = useAuth();
     const [currentTab, setCurrentTab] = React.useState(0)
+    const [pageLoading, setPageLoading] = React.useState(true)
 
     const filteredQuizzes = quizzes.slice().reverse().filter((quiz: any) => {
         if (currentTab === 0) {
@@ -35,8 +36,9 @@ const History = ({ route }: any) => {
             }
         } catch (error) {
             console.error(error);
+        } finally {
+            setPageLoading(false)
         }
-
     }
 
     const deleteQuiz = async (quizId: string) => {
@@ -94,33 +96,36 @@ const History = ({ route }: any) => {
                     />
                 </View>
                 {
-                    filteredQuizzes && filteredQuizzes.length > 0 ? filteredQuizzes.map((quiz: any, index: number) => {
-                        return (
-                            <TouchableOpacity onPress={() => handleQuiz(quiz)} activeOpacity={0.5} key={index} style={{ gap: 6, paddingVertical: 10, paddingHorizontal: 12, borderBottomWidth: 1, borderBottomColor: "#ddd" }}>
-                                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                                    <CustomText>Topic: <CustomText font="medium" variant="bodyLarge">{quiz.topicName}</CustomText></CustomText>
-                                    <CustomText>Date: <CustomText font="medium">{quiz.dateTaken.substring(0, 10)}</CustomText></CustomText>
-                                </View>
-                                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                                    <CustomText>Total Questions: <CustomText font="medium" variant="bodyLarge">{quiz.totalQuestions}</CustomText></CustomText>
-                                    <CustomText>Difficulty: <DifficultyText difficulty={quiz.difficulty} /></CustomText>
-                                </View>
-                                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                                    <CustomText>Score: <CustomText font="medium" variant="bodyLarge">{quiz.correctAnswers}</CustomText></CustomText>
-                                    <TouchableOpacity style={{ borderRadius: 5, borderColor: "red", borderWidth: 1, paddingHorizontal: 10, paddingVertical: 5 }} onPress={() => deleteQuiz(quiz.id)}>
-                                        <Icon source="delete" size={22} color="red" />
-                                    </TouchableOpacity>
-                                </View>
-                            </TouchableOpacity>
-                        )
-                    }) :
-                        <View style={{ padding: 20 }}>
-                            <CustomText style={{ textAlign: 'center' }} variant="bodyLarge">
-                                {currentTab === 0
-                                    ? 'No completed quizzes found'
-                                    : 'No pending quizzes found'}
-                            </CustomText>
-                        </View>
+                    pageLoading ? <View style={{ padding: 20 }}>
+                        <ActivityIndicator animating={true} color={"indigo"} />
+                    </View> :
+                        filteredQuizzes && filteredQuizzes.length > 0 ? filteredQuizzes.map((quiz: any, index: number) => {
+                            return (
+                                <TouchableOpacity onPress={() => handleQuiz(quiz)} activeOpacity={0.5} key={index} style={{ gap: 6, paddingVertical: 10, paddingHorizontal: 12, borderBottomWidth: 1, borderBottomColor: "#ddd" }}>
+                                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                                        <CustomText>Topic: <CustomText font="medium" variant="bodyLarge">{quiz.topicName}</CustomText></CustomText>
+                                        <CustomText>Date: <CustomText font="medium">{quiz.dateTaken.substring(0, 10)}</CustomText></CustomText>
+                                    </View>
+                                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                                        <CustomText>Total Questions: <CustomText font="medium" variant="bodyLarge">{quiz.totalQuestions}</CustomText></CustomText>
+                                        <CustomText>Difficulty: <DifficultyText difficulty={quiz.difficulty} /></CustomText>
+                                    </View>
+                                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                                        <CustomText>Score: <CustomText font="medium" variant="bodyLarge">{quiz.correctAnswers}</CustomText></CustomText>
+                                        <TouchableOpacity style={{ borderRadius: 5, borderColor: "red", borderWidth: 1, paddingHorizontal: 10, paddingVertical: 5 }} onPress={() => deleteQuiz(quiz.id)}>
+                                            <Icon source="delete" size={22} color="red" />
+                                        </TouchableOpacity>
+                                    </View>
+                                </TouchableOpacity>
+                            )
+                        }) :
+                            <View style={{ padding: 20 }}>
+                                <CustomText style={{ textAlign: 'center' }} variant="bodyLarge">
+                                    {currentTab === 0
+                                        ? 'No completed quizzes found'
+                                        : 'No pending quizzes found'}
+                                </CustomText>
+                            </View>
                 }
             </View>
         </BGView>
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,7 +4,7 @@ import CustomButton from "../components/CustomButton";
 import useAuth from "../hooks/useAuth";
 import { AuthAPI } from "../helpers/auth";
 import BGView from "../components/BGView";
-import { Appbar } from "react-native-paper";
+import { ActivityIndicator, Appbar } from "react-native-paper";
 import CustomText from "../components/CustomText";
 import { useFocusEffect } from "@react-navigation/native";
 
@@ -12,6 +12,7 @@ const Profile = () => {
     const { logOut, user } = useAuth();
     const [loading, setLoading] = useState(false);
     const [userData, setUserData] = useState<any>(null);
+    const [pageLoading, setPageLoading] = useState(true);
 
     const handleDelete = async () => {
         try {
@@ -28,13 +29,19 @@ const Profile = () => {
     }
 
     const fetchUser = async () => {
-        const res = await AuthAPI.getUser(user?.id);
-        if (res.hasOwnProperty("auth") && !res.auth) {
-            ToastAndroid.show("Session Expired. Login again", ToastAndroid.SHORT);
-            logOut();
-        }
-        if (res) {
-            setUserData(res?.data);
+        try {
+            const res = await AuthAPI.getUser(user?.id);
+            if (res.hasOwnProperty("auth") && !res.auth) {
+                ToastAndroid.show("Session Expired. Login again", ToastAndroid.SHORT);
+                logOut();
+            }
+            if (res) {
+                setUserData(res?.data);
+            }
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setPageLoading(false);
         }
     }
 
@@ -49,30 +56,37 @@ const Profile = () => {
                 <Appbar.Header mode="center-aligned">
                     <Appbar.Content titleStyle={{ fontFamily: "Poppins-Medium" }} title="Profile" />
                 </Appbar.Header>
-                <View style={{ padding: 20, gap: 10, flex: 1 }}>
-                    <View>
-                        <CustomText>Name:</CustomText>
-                        <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.name}</CustomText>
-                    </View>
-                    <View>
-                        <CustomText>Email:</CustomText>
-                        <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.email}</CustomText>
-                    </View>
-                    <View>
-                        <CustomText>Username:</CustomText>
-                        <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.username}</CustomText>
-                    </View>
-                    <View>
-                        <CustomText>Total Quizzes Attended:</CustomText>
-                        <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.quizzes.length}</CustomText>
-                    </View>
-                </View>
-                <View style={{ padding: 20, gap: 10 }}>
-                    <CustomButton onPress={logOut} mode="contained" >
-                        Logout
-                    </CustomButton>
-                    <CustomButton loading={loading} onPress={handleDelete} mode="contained" style={{ backgroundColor: "red" }} >Delete Account</CustomButton>
-                </View>
+                {
+                    pageLoading ? <View style={{ padding: 20 }}>
+                        <ActivityIndicator animating={true} color={"indigo"} />
+                    </View> :
+                        <>
+                            <View style={{ padding: 20, gap: 10, flex: 1 }}>
+                                <View>
+                                    <CustomText>Name:</CustomText>
+                                    <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.name}</CustomText>
+                                </View>
+                                <View>
+                                    <CustomText>Email:</CustomText>
+                                    <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.email}</CustomText>
+                                </View>
+                                <View>
+                                    <CustomText>Username:</CustomText>
+                                    <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.username}</CustomText>
+                                </View>
+                                <View>
+                                    <CustomText>Total Quizzes Attended:</CustomText>
+                                    <CustomText font="medium" style={{ fontSize: 20 }} >{userData?.quizzes.length}</CustomText>
+                                </View>
+                            </View>
+                            <View style={{ padding: 20, gap: 10 }}>
+                                <CustomButton onPress={logOut} mode="contained" >
+                                    Logout
+                                </CustomButton>
+                                <CustomButton loading={loading} onPress={handleDelete} mode="contained" style={{ backgroundColor: "red" }} >Delete Account</CustomButton>
+                            </View>
+                        </>
+                }
             </View>
         </BGView>
     );
